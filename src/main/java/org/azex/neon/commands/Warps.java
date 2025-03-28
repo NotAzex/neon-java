d87243da8542f484f9a39551845e2067bb91b20e
NotAzex <azexdid@gmail.com>
2025-03-28 17:04:13 +0200

    Removed useless import + cache removal test

--- src/main/java/org/azex/neon/commands/Warps.java
+++ src/main/java/org/azex/neon/commands/Warps.java
@@ -11,8 +11,6 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.IOException;
-
 public class Warps implements CommandExecutor {
 
     private LocationManager locationManager;
